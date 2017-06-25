5bb845f4e12fd43ca73a396fc308dafefb22d613
mosh7890 <mosh7890@gmail.com>
2017-06-25 11:44:06 +0300

    Finished Comment Delete

--- public/script.js
+++ public/script.js
@@ -1,21 +1,68 @@
+var getFromLocalStorage = function () {
+    return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+}
+
+var saveToLocalStorage = function () {
+    localStorage.setItem(STORAGE_ID, JSON.stringify(entries));
+}
+
+var clearLocalStorage = function () {
+    localStorage.clear();
+    entries = getFromLocalStorage();
+}
+
+var STORAGE_ID = 'weather_app';
+
+// Load Data from Local Storage into Array
+var entries = getFromLocalStorage();
+
+var weatherTemplate;
+var commentTemplate;
+
+// Get Handlebars Templates Once and Render Page on Load
+$.get('weather-results.hbs', function (source) {
+    weatherTemplate = Handlebars.compile(source);
+    $.get('weather-comments.hbs', function (source) {
+        commentTemplate = Handlebars.compile(source);
+        renderWeather();
+    }, 'html');
+}, 'html');
+
+var renderWeather = function () {
+    $('.myResults1').empty();
+    for (var i = 0; i < entries.length; i++) {
+        var newHTML = weatherTemplate(entries[i]);
+        $('.myResults1').append(newHTML);
+        renderComments(i);
+    }
+}
+
+var renderComments = function (weatherIndex) {
+    var weather = $('.weather')[weatherIndex];
+    var comments = $(weather).find('.comments-list');
+    comments.empty();
+    for (var i = 0; i < entries[weatherIndex].comments.length; i++) {
+        var newHTML = commentTemplate(entries[weatherIndex].comments[i]);
+        comments.append(newHTML);
+    }
+}
+
 var fetch = function (url) {
-    $.ajaxSetup({
-        beforeSend: function () {
-            $(".loading").show();
-        },
-        complete: function () {
-            $(".loading").hide();
-        }
-    })
     $.ajax({
         method: "GET",
         url: url,
+        beforeSend: function () {
+            $(".loading").show();
+        },
         success: function (data) {
             addToEntries(data);
-            render();
+            renderWeather();
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
+        },
+        complete: function () {
+            $(".loading").hide();
         }
     });
 };
@@ -31,87 +78,63 @@ var addToEntries = function (data) {
         city: data.name,
         country: data.sys.country,
         temperature: temp,
-        windSpeed: data.wind.speed,
         comments: []
     }
     entries.push(tempObj);
     saveToLocalStorage();
 }
 
-var render = function () {
-    $('.myResults1').empty();
-    $.get('weather-results.hbs', function (source) {
-        var template = Handlebars.compile(source);
-        var newHTML = template(entries);
-        $('.myResults1').append(newHTML);
-    }, 'html');
-}
-
-var deleteFromEntries = function (element) {
-    var temp = $(element).closest('.resultsList');
-    var index = temp.index();
+var deleteWeather = function (element) {
+    var index = $(element).closest('.weather').index();
     entries.splice(index, 1);
-    render();
+    renderWeather();
     saveToLocalStorage();
 }
 
-var toggleComments = function (element) {
-    var temp = $(element).closest('.resultsList');
-    temp = $(temp).find('.commentContainer');
-    $(temp).toggle();
-}
-
 var addComment = function (element, data) {
-    var temp = $(element).closest('.resultsList');
-    var index = temp.index();
+    var index = $(element).closest('.weather').index();
     entries[index].comments.push(data);
     saveToLocalStorage();
 }
 
-var saveToLocalStorage = function () {
-    localStorage.setItem(STORAGE_ID, JSON.stringify(entries));
+var deleteComment = function (element) {
+    var entryIndex = $(element).closest('.weather').index();
+    var commentsIndex = $(element).closest('.comment').index();
+    entries[entryIndex].comments.splice(commentsIndex, 1);
+    renderComments(entryIndex);
+    saveToLocalStorage();
 }
 
-var getFromLocalStorage = function () {
-    return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-}
-
-var clearLocalStorage = function () {
-    localStorage.clear();
-    entries = getFromLocalStorage();
-}
-
-var STORAGE_ID = 'weather_app';
-var entries = getFromLocalStorage();
-
-render();
-
-$('.mySearchBtn1').on('click', function () {
-    var input = $('#myInput1').val();
+$('.searchCity').on('click', function () {
+    var input = $('.cityInput').val();
     var apiKey = '&APPID=d703871f861842b79c60988ccf3b17ec';
     var completeUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + input + apiKey;
     fetch(completeUrl);
 });
 
-$('.myClearBtn1').on('click', function () {
+$('.clearLocalStorage').on('click', function () {
     clearLocalStorage();
-    render();
-});
-
-$('.myResults1').on('click', '.commentIcon1', function () {
-    toggleComments(this);
+    renderWeather();
 });
 
 $('.myResults1').on('click', '.trashIcon1', function () {
-    deleteFromEntries(this);
+    deleteWeather(this);
 });
 
-$('.myResults1').on('click', '.myCommentBtn1', function () {
-    var input = $(this).siblings('#myInput2').val();
+$('.myResults1').on('click', '.postNewComment', function () {
+    var input = $(this).siblings('.newComment').val();
     addComment(this, input);
-    render();
-    toggleComments(this);
+    var index = $(this).closest('.weather').index();
+    renderComments(index);
+    $(this).closest('.weather').find('.comments-container').show();
 });
 
+$('.myResults1').on('click', '.trashIcon2', function () {
+    deleteComment(this);
+});
 
+$('.myResults1').on('click', '.commentIcon1', function () {
+    $(this).closest('.weather').find('.comments-container').toggle();
+    $(this).closest('.weather').siblings().find('.comments-container').hide();
 
+});
--- server.js
+++ server.js
@@ -5,9 +5,6 @@ var app = express();
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
-// app.get('/', function (req, res) {
-//     res.send();
-// });
 
 app.listen(8000, function () {
     console.log('Example app listening on port 8000!');
